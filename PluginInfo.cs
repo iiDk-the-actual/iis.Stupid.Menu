87a9a35236fa9976acef653bc433cf23673e9c94
TSCIAN <iankeunen7@gmail.com>
2024-12-07 13:58:25 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,7 +3,7 @@
     public class PluginInfo
     {
         public const string GUID = "org.iidk.gorillatag.iimenu";
-        public const string Name = "ii's Stupid Menu";
+        public const string Name = "LITER MENU";
         public const string Description = "Created by @goldentrophy with love <3";
         public const string Version = "5.1.2";
     }
