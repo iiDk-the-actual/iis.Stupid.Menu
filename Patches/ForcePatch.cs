d0edc75f9d3c4d3b5ed35749d579721478158b7a
iiDk <@>
2025-03-16 16:24:36 -0400

    Fix for disable wind

---
+++ Patches/ForcePatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(ForceVolume), "OnTriggerEnter")]
+    public class ForcePatch
+    {
+        public static bool enabled = false;
+
+        public static bool Prefix()
+        {
+            return !enabled;
+        }
+    }
+
+    [HarmonyPatch(typeof(ForceVolume), "OnTriggerExit")]
+    public class ForcePatch2
+    {
+        public static bool Prefix()
+        {
+            return !ForcePatch.enabled;
+        }
+    }
+
+    [HarmonyPatch(typeof(ForceVolume), "OnTriggerStay")]
+    public class ForcePatch3
+    {
+        public static bool Prefix()
+        {
+            return !ForcePatch.enabled;
+        }
+    }
+}
