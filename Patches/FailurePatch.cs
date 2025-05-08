e7a0508115873a26fa94b6bb8344af1d86d85799
iiDk <@>
2025-05-07 22:38:31 -0400

    Ban reporting API

---
+++ Patches/FailurePatch.cs
@@ -0,0 +1,16 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+using iiMenu.Classes;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(GorillaComputer), "GeneralFailureMessage")]
+    public class FailurePatch
+    {
+        public static void Prefix(string failMessage)
+        {
+            if (failMessage.Contains("YOUR ACCOUNT"))
+                CoroutineManager.instance.StartCoroutine(iiMenu.Menu.Main.ReportFailureMessage(failMessage));
+        }
+    }
+}
