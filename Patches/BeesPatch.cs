d1f9837bab1f631ad93622512ee59513d1900d27
iiDk <@>
2025-05-23 19:36:59 -0400

    Anti sting

---
+++ Patches/BeesPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(AngryBeeSwarm), "RiseGrabbedLocalPlayer")]
+    public class BeesPatch
+    {
+        public static bool enabled = false;
+
+        public static bool Prefix()
+        {
+            if (enabled)
+                return false;
+            
+            return true;
+        }
+    }
+}
