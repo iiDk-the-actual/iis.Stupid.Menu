3e043d9f8f53408b309811b24000cdf19ad2365b
iiDk <@>
2025-05-25 14:26:07 -0400

    Fix AssemblyConfiguration to be accurate depending on build type

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,11 @@ using BepInEx;
 using System.Reflection;
 
 [assembly: AssemblyCompany("goldentrophy")]
+#if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyFileVersion(iiMenu.PluginInfo.Version + ".0")]
 [assembly: AssemblyInformationalVersion(iiMenu.PluginInfo.Version)]
 [assembly: AssemblyProduct(iiMenu.PluginInfo.Name)]
