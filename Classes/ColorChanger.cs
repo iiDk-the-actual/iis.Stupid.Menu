4110db6eea0743f2805d4851a95ecef82d3036e9
iiDk <@>
2025-05-25 22:59:15 -0400

    Migrate from TimedBehaviour to MonoBehaviour

--- Classes/ColorChanger.cs
+++ Classes/ColorChanger.cs
@@ -6,56 +6,44 @@ using UnityEngine;
 
 namespace iiMenu.Classes
 {
-    public class ColorChanger : TimedBehaviour
+    public class ColorChanger : MonoBehaviour
     {
-        public override void Start()
+        public void Start()
         {
-            base.Start();
             gameObjectRenderer = base.GetComponent<Renderer>();
             Update();
         }
 
-        public override void Update()
+        public void Update()
         {
-            base.Update();
             if (colors != null)
             {
-                if (!iiMenu.Menu.Main.dynamicGradients)
+                if (!Main.dynamicGradients)
                 {
                     if (!isMonkeColors)
                     {
-                        if (timeBased)
-                        {
-                            //color = colors.Evaluate(progress);
-                            color = colors.Evaluate((Time.time / 2f) % 1);
-                        }
+                        color = colors.Evaluate((Time.time / 2f) % 1);
+
                         if (isRainbow)
                         {
                             float h = (Time.frameCount / 180f) % 1f;
-                            color = UnityEngine.Color.HSVToRGB(h, 1f, 1f);
+                            color = Color.HSVToRGB(h, 1f, 1f);
                         }
                         if (isPastelRainbow)
                         {
                             float h = (Time.frameCount / 180f) % 1f;
-                            color = UnityEngine.Color.HSVToRGB(h, 0.3f, 1f);
+                            color = Color.HSVToRGB(h, 0.3f, 1f);
                         }
                         if (isEpileptic)
-                        {
                             color = new Color32((byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), 255);
-                        }
                         gameObjectRenderer.material.color = color;
                     }
                     else
                     {
                         if (!Menu.Main.PlayerIsTagged(GorillaTagger.Instance.offlineVRRig))
-                        {
                             gameObjectRenderer.material.color = GorillaTagger.Instance.offlineVRRig.mainSkin.material.color;
-                        }
                         else
-                        {
                             gameObjectRenderer.material.color = new Color32(255, 111, 0, 255);
-                        }
-
                     }
                 }
                 else
@@ -72,7 +60,7 @@ namespace iiMenu.Classes
                             gameObjectRenderer.material.SetFloat("_Glossiness", 0f);
                             gameObjectRenderer.material.SetFloat("_Metallic", 0f);
 
-                            gameObjectRenderer.material.mainTexture = iiMenu.Menu.Main.GetGradientTexture(colors.colorKeys[0].color, colors.colorKeys[1].color);
+                            gameObjectRenderer.material.mainTexture = Main.GetGradientTexture(colors.colorKeys[0].color, colors.colorKeys[1].color);
                         }
                     }
                 }
@@ -82,7 +70,6 @@ namespace iiMenu.Classes
         public Renderer gameObjectRenderer;
         public Gradient colors = null;
         public Color32 color;
-        public bool timeBased = true;
         public bool isRainbow = false;
         public bool isPastelRainbow = false;
         public bool isEpileptic = false;
