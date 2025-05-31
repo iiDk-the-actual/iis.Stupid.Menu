9159de86f2ccdb3f5c221d2b3849af2cdf1fe3f8
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-05-30 23:41:06 -0400

    Make assets class public

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-05-31T01:42:37Z";
+        public const string BuildTimestamp = "2025-05-31T03:27:44Z";
         public const string Version = "6.2.0";
 
 #if DEBUG
