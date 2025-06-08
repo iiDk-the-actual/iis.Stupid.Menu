659f62fffaaeebdae7aba168d29809133566cbca
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-08 01:25:04 -0400

    Build time

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-06-07T19:20:04Z";
+        public const string BuildTimestamp = "2025-06-08T05:25:01Z";
         public const string Version = "6.2.1";
 
 #if DEBUG
