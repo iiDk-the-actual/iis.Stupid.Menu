70cd6f69a5fee4b53d884ae681dcb02e52096bd1
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-13 16:01:09 -0400

    Fix hand taps

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-06-13T19:49:41Z";
+        public const string BuildTimestamp = "2025-06-13T20:00:56Z";
         public const string Version = "6.3.0";
 
 #if DEBUG
