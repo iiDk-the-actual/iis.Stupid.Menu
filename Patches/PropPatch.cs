7488b36660ee43f1946addd645e09286941b56f2
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-15 05:50:41 -0400

    Infinite Prop Distance and Prop Noclip

---
+++ Patches/PropPatch.cs
@@ -0,0 +1,23 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+using iiMenu.Classes;
+using iiMenu.Mods;
+using Photon.Pun;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(PropHauntHandFollower), "GeoCollisionPoint")]
+    public class PropPatch
+    {
+        public static bool enabled = false;
+
+        public static void Postfix(ref Vector3 __result, Vector3 sourcePos, Vector3 targetPos)
+        {
+            if (enabled)
+                __result = targetPos;
+        }
+    }
+}
