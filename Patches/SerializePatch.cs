79bb26b8ed5f702641db9e850b49272ad073f50c
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-22 19:26:22 -0400

    OnSerialize event

---
+++ Patches/SerializePatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(PhotonNetwork), "RunViewUpdate")]
+    public class SerializePatch
+    {
+        public static event System.Action OnSerialize;
+
+        public static void Postfix() =>
+            OnSerialize?.Invoke();
+    }
+}
