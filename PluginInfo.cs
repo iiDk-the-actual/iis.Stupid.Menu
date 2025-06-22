aae38019945eb389794501911f4af90656a53eab
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-22 01:07:45 -0400

    Fix the snowball genitalia mods

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-06-22T04:58:56Z";
+        public const string BuildTimestamp = "2025-06-22T05:07:42Z";
         public const string Version = "6.4.0";
 
 #if DEBUG
