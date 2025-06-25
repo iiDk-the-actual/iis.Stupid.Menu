a05aecabd1af5fc2261832b37a2289acefaa1920
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-25 02:26:23 -0400

    Increase head size on ghost view

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-06-25T06:22:32Z";
+        public const string BuildTimestamp = "2025-06-25T06:26:17Z";
         public const string Version = "6.4.0";
 
 #if DEBUG
