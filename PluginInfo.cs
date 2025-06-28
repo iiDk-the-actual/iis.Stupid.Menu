bf6267af6870e2ea8e2203c36c9f9f339b1a59a1
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-28 18:42:04 -0400

    Remove pointless mod

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-06-28T22:39:28Z";
+        public const string BuildTimestamp = "2025-06-28T22:42:02Z";
         public const string Version = "6.4.2";
 
 #if DEBUG
