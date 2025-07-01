7dd7f61097e783a8b5473551292418a8aa1f6070
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-01 00:08:49 -0400

    Fix tag lag detector

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-01T04:01:40Z";
+        public const string BuildTimestamp = "2025-07-01T04:08:48Z";
         public const string Version = "6.4.2";
 
         public const string BaseDirectory = "iisStupidMenu";
