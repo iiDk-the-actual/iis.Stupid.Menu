67477366307cfbae3312c5d5dce9f96be8d29f37
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-01 00:44:28 -0400

    Fix force toggle again

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-01T04:29:38Z";
+        public const string BuildTimestamp = "2025-07-01T04:44:18Z";
         public const string Version = "6.4.2";
 
         public const string BaseDirectory = "iisStupidMenu";
