fa4bdd5734a4baef97c7b62ce5ae91b9026b5afb
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2025-07-03 17:49:03 -0500

    fix wasd fly colliding with menu again again

--- Mods/Movement.cs
+++ Mods/Movement.cs
@@ -569,65 +569,68 @@ namespace iiMenu.Mods
             bool Space = UnityInput.Current.GetKey(KeyCode.Space);
             bool Ctrl = UnityInput.Current.GetKey(KeyCode.LeftControl);
 
-            if (Mouse.current.rightButton.isPressed && !UnityInput.Current.GetKey(KeyCode.Q))
+            if (!menu)
             {
-                Transform parentTransform = GTPlayer.Instance.rightControllerTransform.parent;
-                Quaternion currentRotation = parentTransform.rotation;
-                Vector3 euler = currentRotation.eulerAngles;
-
-                if (startX < 0)
+                if (Mouse.current.rightButton.isPressed)
                 {
-                    startX = euler.y;
-                    subThingy = Mouse.current.position.value.x / Screen.width;
+                    Transform parentTransform = GTPlayer.Instance.rightControllerTransform.parent;
+                    Quaternion currentRotation = parentTransform.rotation;
+                    Vector3 euler = currentRotation.eulerAngles;
+
+                    if (startX < 0)
+                    {
+                        startX = euler.y;
+                        subThingy = Mouse.current.position.value.x / Screen.width;
+                    }
+                    if (startY < 0)
+                    {
+                        startY = euler.x;
+                        subThingyZ = Mouse.current.position.value.y / Screen.height;
+                    }
+
+                    float newX = startY - ((((Mouse.current.position.value.y / Screen.height) - subThingyZ) * 360) * 1.33f);
+                    float newY = startX + ((((Mouse.current.position.value.x / Screen.width) - subThingy) * 360) * 1.33f);
+
+                    newX = (newX > 180f) ? newX - 360f : newX;
+                    newX = Mathf.Clamp(newX, -90f, 90f);
+
+                    parentTransform.rotation = Quaternion.Euler(newX, newY, euler.z);
                 }
-                if (startY < 0)
+                else
                 {
-                    startY = euler.x;
-                    subThingyZ = Mouse.current.position.value.y / Screen.height;
+                    startX = -1;
+                    startY = -1;
                 }
 
-                float newX = startY - ((((Mouse.current.position.value.y / Screen.height) - subThingyZ) * 360) * 1.33f);
-                float newY = startX + ((((Mouse.current.position.value.x / Screen.width) - subThingy) * 360) * 1.33f);
+                float speed = flySpeed;
+                if (UnityInput.Current.GetKey(KeyCode.LeftShift))
+                    speed *= 2f;
 
-                newX = (newX > 180f) ? newX - 360f : newX;
-                newX = Mathf.Clamp(newX, -90f, 90f);
+                if (W)
+                    GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.forward * Time.deltaTime * speed;
 
-                parentTransform.rotation = Quaternion.Euler(newX, newY, euler.z);
+                if (S)
+                    GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.forward * Time.deltaTime * -speed;
+
+                if (A)
+                    GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.right * Time.deltaTime * -speed;
+
+                if (D)
+                    GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.right * Time.deltaTime * speed;
+
+                if (Space)
+                    GorillaTagger.Instance.rigidbody.transform.position += new Vector3(0f, Time.deltaTime * speed, 0f);
+
+                if (Ctrl)
+                    GorillaTagger.Instance.rigidbody.transform.position += new Vector3(0f, Time.deltaTime * -speed, 0f);
+
+                VRRig.LocalRig.head.rigTarget.transform.rotation = GorillaTagger.Instance.headCollider.transform.rotation;
+
+                if (!W && !A && !S && !D && !Space && !Ctrl && lastPosition != Vector3.zero)
+                    GorillaTagger.Instance.rigidbody.transform.position = lastPosition;
+                else
+                    lastPosition = GorillaTagger.Instance.rigidbody.transform.position;
             }
-            else
-            {
-                startX = -1;
-                startY = -1;
-            }
-
-            float speed = flySpeed;
-            if (UnityInput.Current.GetKey(KeyCode.LeftShift))
-                speed *= 2f;
-
-            if (W)
-                GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.forward * Time.deltaTime * speed;
-
-            if (S)
-                GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.forward * Time.deltaTime * -speed;
-
-            if (A)
-                GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.right * Time.deltaTime * -speed;
-
-            if (D)
-                GorillaTagger.Instance.rigidbody.transform.position += GTPlayer.Instance.rightControllerTransform.parent.right * Time.deltaTime * speed;
-
-            if (Space)
-                GorillaTagger.Instance.rigidbody.transform.position += new Vector3(0f, Time.deltaTime * speed, 0f);
-
-            if (Ctrl)
-                GorillaTagger.Instance.rigidbody.transform.position += new Vector3(0f, Time.deltaTime * -speed, 0f);
-
-            VRRig.LocalRig.head.rigTarget.transform.rotation = GorillaTagger.Instance.headCollider.transform.rotation;
-
-            if (!W && !A && !S && !D && !Space && !Ctrl && lastPosition != Vector3.zero)
-                GorillaTagger.Instance.rigidbody.transform.position = lastPosition;
-            else
-                lastPosition = GorillaTagger.Instance.rigidbody.transform.position;
         }
 
         private static float driveSpeed = 0f;
