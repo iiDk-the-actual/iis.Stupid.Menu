b6ad64b16128cc6ab9683f032126cfc198965cd2
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-03 03:44:08 -0400

    Disable security checks for friends

---
+++ Patches/Menu/NamePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using iiMenu.Classes;
+using System;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(VRRig), "UpdateName", new Type[] { typeof(bool) })]
+    public class NamePatch
+    {
+        private static void Postfix(VRRig __instance, bool isNamePermissionEnabled)
+        {
+            NetPlayer player = RigManager.GetPlayerFromVRRig(__instance) ?? null;
+            if (__instance != VRRig.LocalRig && player != null && FriendManager.IsPlayerFriend(player))
+            {
+                __instance.playerText1.text = player.NickName;
+                __instance.playerText2.text = Menu.Main.NoRichtextTags(player.NickName);
+            }
+        }
+    }
+}
