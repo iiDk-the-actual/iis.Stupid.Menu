2e6bd7a736c8704b1d58a2cd1b656b941de7bf60
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-05 16:42:47 -0400

    Fix disable network triggers

---
+++ Patches/Menu/NetworkTriggerPatch.cs
@@ -0,0 +1,15 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+using Photon.Pun;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(GorillaNetworkJoinTrigger), "OnBoxTriggered")]
+    public class NetworkTriggerPatch
+    {
+        public static bool enabled;
+
+        public static bool Prefix() =>
+            !enabled;
+    }
+}
