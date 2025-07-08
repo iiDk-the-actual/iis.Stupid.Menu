cc6a0d9e902594e5397b1a310fe309b3712fad2b
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-07 21:48:07 -0400

    Optimize GetComponent usage

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-08T01:41:42Z";
+        public const string BuildTimestamp = "2025-07-08T01:46:25Z";
         public const string Version = "6.5.1";
 
         public const string BaseDirectory = "iisStupidMenu";
