a402bae6faffe3a88adb0e613fe1dc3c21328207
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-09 23:29:36 -0400

    Small keyboard delay

--- Classes/KeyboardCollider.cs
+++ Classes/KeyboardCollider.cs
@@ -6,12 +6,15 @@ namespace iiMenu.Classes
     public class KeyboardKey : MonoBehaviour
 	{
 		public string key;
+		public static float delay;
 		
 		public void OnTriggerEnter(Collider collider)
 		{
-			if ((collider == lKeyCollider || collider == rKeyCollider) && menu != null)
+			if ((collider == lKeyCollider || collider == rKeyCollider) && menu != null && Time.time > delay)
 			{
-				if (doButtonsVibrate)
+				delay = Time.time + 0.1f;
+
+                if (doButtonsVibrate)
 					GorillaTagger.Instance.StartVibration(collider == lKeyCollider, GorillaTagger.Instance.tagHapticStrength / 2f, GorillaTagger.Instance.tagHapticDuration / 2f);
 				
                 VRRig.LocalRig.PlayHandTapLocal(66, collider == lKeyCollider, buttonClickVolume / 10f);
