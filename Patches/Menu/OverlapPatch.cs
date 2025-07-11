46b589023742cb0589934f3d615ef1361bd41446
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-11 02:33:08 -0400

    Overlap building

---
+++ Patches/Menu/OverlapPatch.cs
@@ -0,0 +1,18 @@
+﻿using GorillaLocomotion;
+using GorillaTagScripts;
+using HarmonyLib;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(BuilderAttachGridPlane), "IsConnected")]
+    public class OverlapPatch
+    {
+        public static bool enabled;
+
+        public static void Postfix(BuilderAttachGridPlane __instance, ref bool __result, SnapBounds bounds)
+        {
+            if (enabled)
+                __result = false;
+        }
+    }
+}
