b11ba3681bd26a580e539b1f17f5ab2f585a42c3
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-11 17:45:20 -0400

    Fix menu with one harmony patch (I hate myself)

--- Patches/Menu/RigPatch.cs
+++ Patches/Menu/RigPatch.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using GorillaNetworking;
+using HarmonyLib;
+using System;
 
 namespace iiMenu.Patches
 {
@@ -9,9 +11,16 @@ namespace iiMenu.Patches
             !__instance.isLocal;
     }
 
+    [HarmonyPatch(typeof(VRRig), "Awake")]
+    public class RigPatch2
+    {
+        public static bool Prefix(VRRig __instance) =>
+            __instance.gameObject.name != "Local Gorilla Player(Clone)";
+    }
+
     // Thanks nugget for help with patch
     [HarmonyPatch(typeof(VRRigJobManager), "DeregisterVRRig")]
-    public static class RigPatch2
+    public static class RigPatch3
     {
         public static bool Prefix(VRRigJobManager __instance, VRRig rig) =>
             !rig.isLocal;
