958a5ee68828ff3b38f38d1bc2da33360b889a51
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-14 00:55:55 -0400

    Serialize patch overlap property

--- Patches/Menu/SerializePatch.cs
+++ Patches/Menu/SerializePatch.cs
@@ -1,14 +1,26 @@
 ﻿using HarmonyLib;
 using Photon.Pun;
+using System;
 
 namespace iiMenu.Patches
 {
     [HarmonyPatch(typeof(PhotonNetwork), "RunViewUpdate")]
     public class SerializePatch
     {
-        public static event System.Action OnSerialize;
+        public static event Action OnSerialize;
+        public static Func<bool> OverrideSerialization;
 
-        public static void Postfix() =>
+        public static bool Prefix()
+        {
+            if (!PhotonNetwork.InRoom)
+                return true;
+
             OnSerialize?.Invoke();
+
+            if (OverrideSerialization == null)
+                return true;
+            else
+                return OverrideSerialization();
+        }
     }
 }
