1cb8fbf3e2a7ccdc8a6646d3fdbac39881b1df45
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-14 05:42:35 -0400

    Joined room patch for bypass join room type

---
+++ Patches/Menu/JoinedRoomPatch.cs
@@ -0,0 +1,24 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(PhotonNetworkController), "JoinedRoomPatch")]
+    internal class JoinedRoomPatch
+    {
+        public static bool enabled;
+
+        private static void Prefix()
+        {
+            if (enabled)
+                PhotonNetworkController.Instance.currentJoinType = JoinType.FollowingParty;
+        }
+    }
+}
