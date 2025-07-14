c868616d503f3650f6cf17b5191fa11e6c1dabfa
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-14 05:41:45 -0400

    Fix hand tap patch

---
+++ Patches/Menu/EffectDataPatch.cs
@@ -0,0 +1,64 @@
+﻿using HarmonyLib;
+using iiMenu.Menu;
+using Photon.Pun;
+using UnityEngine;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(VRRig), "SetHandEffectData")]
+    public class EffectDataPatch
+    {
+        public static bool enabled;
+        public static bool tapsEnabled = true;
+        public static bool doOverride;
+        public static float overrideVolume = 99999f;
+        public static int tapMultiplier = 1;
+
+        private static bool Prefix(VRRig __instance, HandEffectContext effectContext, int audioClipIndex, bool isDownTap, bool isLeftHand, float handTapVolume, float handTapSpeed, Vector3 dirFromHitToHand)
+        {
+            if (enabled)
+            {
+                if (__instance.isLocal)
+                {
+                    if (doOverride)
+                    {
+                        effectContext.speed = overrideVolume;
+                        effectContext.soundVolume = overrideVolume;
+
+                        if (PhotonNetwork.InRoom)
+                        {
+                            if (tapMultiplier > 1)
+                            {
+                                for (int i = 0; i < tapMultiplier; i++)
+                                {
+                                    GorillaTagger.Instance.myVRRig.SendRPC("RPC_PlayHandTap", RpcTarget.All, new object[]
+                                    {
+                                        audioClipIndex,
+                                        isLeftHand,
+                                        handTapSpeed
+                                    });
+                                }
+                                Main.RPCProtection();
+                            }
+                        } else
+                            VRRig.LocalRig.PlayHandTapLocal(audioClipIndex, isLeftHand, overrideVolume);
+
+                        return false;
+                    }
+                    if (!tapsEnabled)
+                    {
+                        effectContext.speed = 0f;
+                        effectContext.soundVolume = 0f;
+
+                        GorillaTagger.Instance.handTapVolume = 0f;
+                        GorillaTagger.Instance.handTapSpeed = 0f;
+                        GorillaTagger.Instance.audioClipIndex = -1;
+
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
