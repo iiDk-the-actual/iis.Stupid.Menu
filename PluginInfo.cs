89f3430188a26691880851a9696d1eefb4342d8a
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-14 20:02:04 -0400

    Update lines

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-14T09:40:59Z";
+        public const string BuildTimestamp = "2025-07-14T23:54:01Z";
         public const string Version = "6.7.0";
 
         public const string BaseDirectory = "iisStupidMenu";
