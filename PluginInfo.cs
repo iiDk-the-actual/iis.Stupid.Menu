308387e2f44aea8e1f09615871c3d37a08936a58
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-17 14:16:23 -0400

    Fix get bug for whne rig disabled

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-17T17:18:09Z";
+        public const string BuildTimestamp = "2025-07-17T18:16:17Z";
         public const string Version = "6.7.0";
 
         public const string BaseDirectory = "iisStupidMenu";
