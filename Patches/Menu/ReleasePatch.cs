b07c21d59041e31ead5ed8830bc84b416cfb1c4c
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-18 15:03:25 -0400

    Fix release patch

--- Patches/Menu/ReleasePatch.cs
+++ Patches/Menu/ReleasePatch.cs
@@ -13,34 +13,28 @@ namespace iiMenu.Patches
         {
             if (enabled)
             {
+                if (!__instance.OnRelease(zoneReleased, releasingHand))
+                    __result = false;
+                
                 if (!__instance.myRig.isOfflineVRRig)
                 {
-                    HandLink handLink = ((releasingHand == EquipmentInteractor.instance.leftHand) ? VRRig.LocalRig.leftHandLink : VRRig.LocalRig.rightHandLink);
-                    bool targetNotGrounded = false;
+                    bool grounded = false;
 
-                    if (GTPlayer.Instance.IsGroundedHand)
-                    {
-                        if (!__instance.isGroundedHand)
-                            targetNotGrounded = true;
-                    }
-                    else if (GTPlayer.Instance.IsGroundedButt)
-                    {
-                        if (!__instance.isGroundedHand && !__instance.isGroundedButt)
-                            targetNotGrounded = true;
-                    }
+                    HandLink handLink = ((releasingHand == EquipmentInteractor.instance.leftHand) ? VRRig.LocalRig.leftHandLink : VRRig.LocalRig.rightHandLink);
+                   
+                    HandLinkAuthorityStatus selfHandLinkAuthority = GTPlayer.Instance.GetSelfHandLinkAuthority();
+                    HandLinkAuthorityStatus selfChainAuthority = handLink.GetChainAuthority(out int selfAuthority);
+
+                    if (selfHandLinkAuthority.type >= HandLinkAuthorityType.ButtGrounded && selfChainAuthority.type < selfHandLinkAuthority.type)
+                        grounded = true;
                     else if (handLink.myOtherHandLink.grabbedLink != null)
                     {
-                        handLink.GetChainAuthority(out int stepsToGroundSelf, out bool groundIsHandSelf, out float lastTouchedGroundTimestampSelf, out float lastHandTouchedGroundTimestampSelf);
-                        handLink.myOtherHandLink.GetChainAuthority(out int stepsToGroundTarget, out bool groundIsHandTarget, out float lastTouchedGroundTimestampTarget, out float lastHandTouchedGroundTimestampTarget);
-
-                        if (groundIsHandSelf && !groundIsHandTarget)
-                            targetNotGrounded = true;
-                        
-                        else if (stepsToGroundSelf < 0 && stepsToGroundTarget >= 0)
-                            targetNotGrounded = true;
+                        HandLinkAuthorityStatus otherChainAuthority = handLink.myOtherHandLink.GetChainAuthority(out int otherAuthority);
+                        if (otherChainAuthority.type >= HandLinkAuthorityType.ButtGrounded && selfChainAuthority.type < otherChainAuthority.type)
+                            grounded = true;
                     }
 
-                    if (targetNotGrounded)
+                    if (grounded)
                     {
                         Vector3 averageVelocity = (handLink.isLeftHand ? GTPlayer.Instance.leftHandCenterVelocityTracker : GTPlayer.Instance.rightHandCenterVelocityTracker).GetAverageVelocity(true, 0.15f, false).normalized * 20f;
                         __instance.myRig.netView.SendRPC("DroppedByPlayer", __instance.myRig.OwningNetPlayer, new object[] { averageVelocity });
@@ -49,7 +43,6 @@ namespace iiMenu.Patches
 
                     handLink.BreakLink();
                 }
-                __result = true;
                 return false;
             }
             
