aacb422cc5f6734cd8e14d42621d579fe5888297
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-28 16:53:28 -0400

    More auto join rooms

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-28T00:42:54Z";
+        public const string BuildTimestamp = "2025-07-28T05:37:17Z";
         public const string Version = "6.8.0";
 
         public const string BaseDirectory = "iisStupidMenu";
