39050c40a3d29a339e04b0437e28e18358aec33a
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-01 00:31:17 -0400

    Make laser not go to 0, 0, 0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-07-31T07:27:55Z";
+        public const string BuildTimestamp = "2025-08-01T04:31:06Z";
         public const string Version = "6.8.1";
 
         public const string BaseDirectory = "iisStupidMenu";
