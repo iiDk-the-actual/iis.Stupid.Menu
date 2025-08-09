1bcc5fb42e9c5473ac747c9a7757fd6d5212159e
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-09 07:15:01 -0400

    Fix random disconnection, hopefully

--- Patches/Menu/OwnershipPatch.cs
+++ Patches/Menu/OwnershipPatch.cs
@@ -11,6 +11,6 @@ namespace iiMenu.Patches
         public static List<RequestableOwnershipGuard> blacklistedGuards = new List<RequestableOwnershipGuard>();
 
         public static bool Prefix(RequestableOwnershipGuard __instance, string nonce, PhotonMessageInfo info) =>
-            enabled && __instance.photonView.IsMine && !blacklistedGuards.Contains(__instance);
+            !enabled || __instance.photonView.IsMine && !blacklistedGuards.Contains(__instance);
     }
 }
