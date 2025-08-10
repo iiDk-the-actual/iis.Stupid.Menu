49286843cb001f3aaf3da55a07e5e8c79b494e3b
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-09 20:48:33 -0400

    Fix bug ownership patch

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-08-10T00:45:51Z";
+        public const string BuildTimestamp = "2025-08-10T00:48:18Z";
         public const string Version = "6.9.0";
 
         public const string BaseDirectory = "iisStupidMenu";
