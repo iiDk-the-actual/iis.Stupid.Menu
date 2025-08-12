b6e16ce838d60b3ec48745ff697b3456f38146c6
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-12 04:41:01 -0400

    Make bypass automod not affected by soundboards

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-08-12T07:50:04Z";
+        public const string BuildTimestamp = "2025-08-12T08:31:25Z";
         public const string Version = "6.9.0";
 
         public const string BaseDirectory = "iisStupidMenu";
