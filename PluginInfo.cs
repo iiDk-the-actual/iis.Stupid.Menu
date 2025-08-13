ac3d1de1c98131ad2907430faebc5a0b282bdcc8
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-13 15:24:09 -0400

    Change FPS Spoof

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-08-13T18:34:05Z";
+        public const string BuildTimestamp = "2025-08-13T19:24:07Z";
         public const string Version = "6.9.0";
 
         public const string BaseDirectory = "iisStupidMenu";
