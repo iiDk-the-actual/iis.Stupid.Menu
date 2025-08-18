ab5eb5616a2cb7879f036c49ad4514d4f4eff389
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-17 23:52:38 -0400

    Fix chicken gun/all

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-08-18T01:28:59Z";
+        public const string BuildTimestamp = "2025-08-18T03:47:25Z";
         public const string Version = "6.9.1";
 
         public const string BaseDirectory = "iisStupidMenu";
