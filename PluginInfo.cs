3b62e7f8e5c563eb22bb3275c5efcb837012665f
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-19 18:54:22 -0400

    Fix miniguns

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-08-19T22:29:19Z";
+        public const string BuildTimestamp = "2025-08-19T22:54:18Z";
         public const string Version = "6.9.1";
 
         public const string BaseDirectory = "iisStupidMenu";
