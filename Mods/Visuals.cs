dde2f93caf9aece5dde1e00e3c686ad506baa1c2
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-21 19:53:36 -0400

    Remove and sort usings

--- Mods/Visuals.cs
+++ Mods/Visuals.cs
@@ -9,7 +9,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 using UnityEngine.Rendering;
 using static iiMenu.Classes.RigManager;
 using static iiMenu.Menu.Main;
