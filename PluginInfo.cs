6e39e372d593d66c902fb1e81fb85a59fe53a828
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-03 22:31:01 -0400

    Fix fix fix rig colors

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3";
-        public const string BuildTimestamp = "2025-09-04T02:27:16Z";
+        public const string BuildTimestamp = "2025-09-04T02:30:43Z";
         public const string Version = "6.9.1";
 
         public const string BaseDirectory = "iisStupidMenu";
