4e926dc0827ceffd50614dc5b9b3c2ee9aee3154
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-14 16:06:10 -0400

    Go away

--- Mods/Settings.cs
+++ Mods/Settings.cs
@@ -691,7 +691,7 @@ namespace iiMenu.Mods
                     },
                     new ButtonInfo {
                         buttonText = "Player FPS",
-                        overlapText = $"FPS: {playerRig.fps}F",
+                        overlapText = $"FPS: {playerRig.fps}",
                         label = true
                     }
                 }
