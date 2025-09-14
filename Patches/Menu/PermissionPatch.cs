8bee29090c9c20a4aaff3f5ad9a143909dc0ab69
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-14 12:37:44 -0400

    Bypass K-ID Restrictions

---
+++ Patches/Menu/PermissionPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+
+namespace iiMenu.Patches
+{
+    [HarmonyPatch(typeof(KIDManager), "HasPermissionToUseFeature")]
+    public class PermissionPatch
+    {
+        public static bool enabled;
+
+        public static void Postfix(ref bool __result)
+        {
+            if (enabled)
+                __result = true;
+        }
+    }
+}
