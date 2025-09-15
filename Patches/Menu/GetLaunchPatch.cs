c4592b72f00a52616ee122aabbbf9ef36534fc22
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-14 22:21:06 -0400

    Improve paintbrawl aimbot with math

--- Patches/Menu/GetLaunchPatch.cs
+++ Patches/Menu/GetLaunchPatch.cs
@@ -1,9 +1,7 @@
 ﻿using HarmonyLib;
-using iiMenu.Classes;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 using static iiMenu.Classes.RigManager;
 using static iiMenu.Menu.Main;
 
@@ -37,32 +35,41 @@ namespace iiMenu.Patches
                     .Select(x => x.Rig)
                     .FirstOrDefault();
 
-                __result = CalcMinSpeed(__instance.centerOrigin.transform.position, targetRig.headMesh.transform.position);
+                __result = CalcMinSpeed(__instance.centerOrigin.transform.position, targetRig);
             }
         }
 
         // ChatGPT used for math.. because I'm only 14 and haven't learned this yet and am not taking a class for a Gorilla Tag mod
-        private static Vector3 CalcMinSpeed(Vector3 origin, Vector3 target)
+        private static Vector3 CalcMinSpeed(Vector3 origin, VRRig targetRig)
         {
-            Vector3 displacement = target - origin;
-            Vector3 displacementXZ = new Vector3(displacement.x, 0, displacement.z);
+            Vector3 targetPos = targetRig.headMesh.transform.position;
+            Vector3 targetVel = targetRig.LatestVelocity();
 
-            float x = displacementXZ.magnitude;
-            float y = displacement.y;
+            Vector3 displacement = targetPos - origin;
+            Vector3 displacementXZ = new Vector3(displacement.x, 0, displacement.z);
+            Vector3 velocityXZ = new Vector3(targetVel.x, 0, targetVel.z);
+
             float g = -Physics.gravity.y;
 
-            float minSpeed = Mathf.Sqrt(g * (y + Mathf.Sqrt(x * x + y * y)));
+            float x = displacementXZ.magnitude;
+            float roughSpeed = 20f;
+            float time = x / roughSpeed;
 
+            Vector3 futurePos = targetPos + targetVel * time;
+            displacement = futurePos - origin;
+            displacementXZ = new Vector3(displacement.x, 0, displacement.z);
+            float y = displacement.y;
+            x = displacementXZ.magnitude;
+
+            float minSpeed = Mathf.Sqrt(g * (y + Mathf.Sqrt(x * x + y * y)));
             float launchSpeed = minSpeed * 2.5f;
 
-            return CalcVelocity(origin, target, launchSpeed);
+            return CalcVelocity(origin, displacement, launchSpeed);
         }
 
-        private static Vector3 CalcVelocity(Vector3 origin, Vector3 target, float speed)
+        private static Vector3 CalcVelocity(Vector3 origin, Vector3 displacement, float speed)
         {
-            Vector3 displacement = target - origin;
             Vector3 displacementXZ = new Vector3(displacement.x, 0, displacement.z);
-
             float x = displacementXZ.magnitude;
             float y = displacement.y;
             float g = -Physics.gravity.y;
@@ -73,13 +80,11 @@ namespace iiMenu.Patches
                 return displacement.normalized * speed;
 
             float sqrt = Mathf.Sqrt(underSqrt);
-
             float angle = Mathf.Atan((v2 - sqrt) / (g * x));
 
             Vector3 dirXZ = displacementXZ.normalized;
             Vector3 result = dirXZ * Mathf.Cos(angle) * speed + Vector3.up * Mathf.Sin(angle) * speed;
             return result;
         }
-
     }
 }
