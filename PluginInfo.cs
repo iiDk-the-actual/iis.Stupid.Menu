9a0420fa9b6a14e3f4a0500181e5f7913f45249d
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-15 22:15:54 -0400

    Fix extreme sensitivity with calcminspeed

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-09-16T01:44:54Z";
+        public const string BuildTimestamp = "2025-09-16T02:15:47Z";
         public const string Version = "7.0.0";
 
         public const string BaseDirectory = "iisStupidMenu";
