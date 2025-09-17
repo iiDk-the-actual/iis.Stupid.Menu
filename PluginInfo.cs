c021f0239dcca4c1961b9ce2210519cdea41b34a
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-16 21:35:11 -0400

    Fix trigger anti report all

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-09-17T01:17:11Z";
+        public const string BuildTimestamp = "2025-09-17T01:35:07Z";
         public const string Version = "7.0.0";
 
         public const string BaseDirectory = "iisStupidMenu";
