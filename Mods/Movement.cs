c788b19d09a44e2f8791fa165f88ee3d9a88d585
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-16 21:39:19 -0400

    Optimize gravity mods

--- Mods/Movement.cs
+++ Mods/Movement.cs
@@ -1350,24 +1350,22 @@ namespace iiMenu.Mods
             GTPlayer.Instance.disableMovement = false;
 
         public static void LowGravity() =>
-            GorillaTagger.Instance.rigidbody.AddForce(Vector3.up * (Time.unscaledDeltaTime * (6.66f / Time.unscaledDeltaTime)), ForceMode.Acceleration);
+            GorillaTagger.Instance.rigidbody.AddForce(Vector3.up * 6.66f, ForceMode.Acceleration);
 
         public static void ZeroGravity() =>
             GorillaTagger.Instance.rigidbody.AddForce(-Physics.gravity, ForceMode.Acceleration);
 
         public static void HighGravity() =>
-            GorillaTagger.Instance.rigidbody.AddForce(Vector3.down * (Time.unscaledDeltaTime * (7.77f / Time.unscaledDeltaTime)), ForceMode.Acceleration);
-        
+            GorillaTagger.Instance.rigidbody.AddForce(Vector3.down * 7.77f, ForceMode.Acceleration);
 
         public static void ReverseGravity()
         {
-            GorillaTagger.Instance.rigidbody.AddForce(Vector3.up * (Time.deltaTime * (19.62f / Time.deltaTime)), ForceMode.Acceleration);
+            GorillaTagger.Instance.rigidbody.AddForce(Vector3.up * 19.62f, ForceMode.Acceleration);
             GTPlayer.Instance.rightControllerTransform.parent.rotation = Quaternion.Euler(180f, 0f, 0f);
         }
 
         public static void UnflipCharacter() =>
             GTPlayer.Instance.rightControllerTransform.parent.rotation = Quaternion.identity;
-        
 
         private static readonly List<object[]> playerPositions = new List<object[]>();
         public static void Rewind()
