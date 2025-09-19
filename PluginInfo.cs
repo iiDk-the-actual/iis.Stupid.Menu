8fa424ca823148bb8bc7a1d8fc7e38fa796f960a
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-19 19:39:53 -0400

    ResourceURL option

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,10 +5,11 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-09-19T23:07:58Z";
+        public const string BuildTimestamp = "2025-09-19T23:38:20Z";
         public const string Version = "7.0.0";
 
         public const string BaseDirectory = "iisStupidMenu";
+        public const string ResourceURL = "https://github.com/iiDk-the-actual/ModInfo/raw/main";
 
 #if DEBUG
         public static bool BetaBuild = true;
