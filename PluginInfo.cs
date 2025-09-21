df548a035d3115412aa0d711c5c42e38416fd1de
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-21 13:59:17 -0400

    Legally required to ship license with menu

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * ii's Stupid Menu  PluginInfo.cs
  * A mod menu for Gorilla Tag with over 1000+ mods
  *
@@ -26,7 +26,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-09-21T17:50:04Z";
+        public const string BuildTimestamp = "2025-09-21T17:58:53Z";
         public const string Version = "7.1.0";
 
         public const string BaseDirectory = "iisStupidMenu";
