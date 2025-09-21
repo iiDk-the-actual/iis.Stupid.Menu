5193a415991e1aecb26ac5fba301188668b98bcc
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-21 13:51:28 -0400

    Legal information

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,3 +1,24 @@
+/*
+ * ii's Stupid Menu  PluginInfo.cs
+ * A mod menu for Gorilla Tag with over 1000+ mods
+ *
+ * Copyright (C) 2025  Goldentrophy Software
+ * https://github.com/iiDk-the-actual/iis.Stupid.Menu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 ﻿namespace iiMenu
 {
     public class PluginInfo
@@ -5,7 +26,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-09-21T17:29:04Z";
+        public const string BuildTimestamp = "2025-09-21T17:50:04Z";
         public const string Version = "7.1.0";
 
         public const string BaseDirectory = "iisStupidMenu";
