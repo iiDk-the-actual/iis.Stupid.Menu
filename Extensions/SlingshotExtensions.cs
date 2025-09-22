18699837117182f14df77b27d94a9ba7893c776a
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-22 19:13:05 -0400

    Remove and sort usings

--- Extensions/SlingshotExtensions.cs
+++ Extensions/SlingshotExtensions.cs
@@ -19,9 +19,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-using GorillaLocomotion;
 using UnityEngine;
-using UnityEngine.Analytics;
 
 namespace iiMenu.Extensions
 {
