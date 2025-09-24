a51cbea8e374620b2b818aabd3d92d376ab23a64
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-23 21:12:35 -0400

    Increase jump trajectories minimum velocity

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-09-24T01:11:13Z";
+        public const string BuildTimestamp = "2025-09-24T01:12:22Z";
         public const string Version = "7.1.0";
 
         public const string BaseDirectory = "iisStupidMenu";
