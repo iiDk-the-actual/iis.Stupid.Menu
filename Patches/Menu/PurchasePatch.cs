9a72107912bc76cc5677b6a060243d214cef07ba
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-23 21:42:52 -0400

    Fake purchasing with infinite shiny rocks

---
+++ Patches/Menu/PurchasePatch.cs
@@ -0,0 +1,60 @@
+/*
+ * ii's Stupid Menu  Patches/Menu/PlacePatch.cs
+ * A mod menu for Gorilla Tag with over 1000+ mods
+ *
+ * Copyright (C) 2025  Goldentrophy Software
+ * https://github.com/iiDk-the-actual/iis.Stupid.Menu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using GorillaNetworking;
+using GorillaTagScripts;
+using HarmonyLib;
+using PlayFab.ClientModels;
+
+namespace iiMenu.Patches.Menu
+{
+    [HarmonyPatch(typeof(CosmeticsController), "PurchaseItem")]
+    public class PurchasePatch
+    {
+        public static bool enabled;
+
+        private static bool Prefix()
+        {
+            if (enabled)
+            {
+                CosmeticsController.CosmeticItem itemFromDict = CosmeticsController.instance.GetItemFromDict(CosmeticsController.instance.itemToBuy.itemName);
+                if (itemFromDict.itemCategory == CosmeticsController.CosmeticCategory.Set)
+                {
+                    CosmeticsController.instance.UnlockItem(CosmeticsController.instance.itemToBuy.itemName, false);
+                    foreach (string item in itemFromDict.bundledItems)
+                        CosmeticsController.instance.UnlockItem(item, false);
+                }
+                else
+                    CosmeticsController.instance.UnlockItem(CosmeticsController.instance.itemToBuy.itemName, false);
+
+                CosmeticsController.instance.UpdateMyCosmetics();
+                CosmeticsController.instance.currentPurchaseItemStage = CosmeticsController.PurchaseItemStages.Success;
+
+                CosmeticsController.instance.UpdateShoppingCart();
+                CosmeticsController.instance.ProcessPurchaseItemState(null, CosmeticsController.instance.isLastHandTouchedLeft);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
