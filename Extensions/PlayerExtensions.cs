7f0eae4c0d6059b91e37610bb7ac993fdd7d653a
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-23 21:03:38 -0400

    Rename to PlayerExtensions

---
+++ Extensions/PlayerExtensions.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * ii's Stupid Menu  Extensions/PlayerExtensions.cs
+ * A mod menu for Gorilla Tag with over 1000+ mods
+ *
+ * Copyright (C) 2025  Goldentrophy Software
+ * https://github.com/iiDk-the-actual/iis.Stupid.Menu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using ExitGames.Client.Photon;
+using iiMenu.Managers;
+using Photon.Realtime;
+
+namespace iiMenu.Extensions
+{
+    public static class PlayerExtensions
+    {
+        public static Player GetPlayer(this NetPlayer self) =>
+            RigManager.NetPlayerToPlayer(self);
+
+        public static VRRig VRRig(this NetPlayer self) =>
+            RigManager.GetVRRigFromPlayer(self);
+
+        public static Hashtable GetCustomProperties(this NetPlayer self) =>
+            self.GetPlayer().CustomProperties;
+
+        public static VRRig VRRig(this Player self) =>
+            RigManager.GetVRRigFromPlayer(self);
+    }
+}
