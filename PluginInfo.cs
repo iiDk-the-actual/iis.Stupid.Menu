2791a9c38a16da4ac279bd68d8e30df4fa6e8a73
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-10-04 14:10:52 -0400

    Fix misspell

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-10-04T05:05:22Z";
+        public const string BuildTimestamp = "2025-10-04T18:10:46Z";
         public const string Version = "7.3.0";
 
         public const string BaseDirectory = "iisStupidMenu";
