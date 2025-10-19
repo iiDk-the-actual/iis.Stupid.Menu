ddbeb0e57e11ece28fb7a645caaa1de890ddba86
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-10-19 19:18:20 -0400

    Make stump kick gun work in public rooms

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-10-19T22:01:11Z";
+        public const string BuildTimestamp = "2025-10-19T23:18:12Z";
         public const string Version = "7.4.0";
 
         public const string BaseDirectory = "iisStupidMenu";
