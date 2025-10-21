5efbbe42575f8aaa7ffbf3f4c4d8659a1f90ceef
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2025-10-21 18:29:07 -0500

    Fix Monke Magic mods (???)

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-10-21T18:00:23Z";
+        public const string BuildTimestamp = "2025-10-21T23:29:01Z";
         public const string Version = "7.5.0";
 
         public const string BaseDirectory = "iisStupidMenu";
