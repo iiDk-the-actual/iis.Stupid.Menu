04ceeb3863128d592da4c9ab3bea0ce3df60dcb7
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2025-10-20 22:05:55 -0500

    Pumpkin Watcher

---
+++ Patches/Menu/WatcherEyesPatch.cs
@@ -0,0 +1,44 @@
+/*
+ * ii's Stupid Menu  Patches/Menu/WatcherEyesPatch.cs
+ * A mod menu for Gorilla Tag with over 1000+ mods
+ *
+ * Copyright (C) 2025  Goldentrophy Software
+ * https://github.com/iiDk-the-actual/iis.Stupid.Menu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using GorillaLocomotion;
+using HarmonyLib;
+using iiMenu.Mods;
+using UnityEngine;
+
+namespace iiMenu.Patches.Menu
+{
+    [HarmonyPatch(typeof(HalloweenWatcherEyes), "Update")]
+    public class WatcherEyesPatch
+    {
+        public static bool enabled;
+
+        public static void Prefix(HalloweenWatcherEyes __instance)
+        {
+            if (enabled)
+            {
+                Quaternion normalized = Quaternion.LookRotation((GTPlayer.Instance.headCollider.transform.position - __instance.transform.position).normalized);
+                __instance.leftEye.transform.rotation = normalized;
+                __instance.rightEye.transform.rotation = normalized;
+            }
+        }
+    }
+}
