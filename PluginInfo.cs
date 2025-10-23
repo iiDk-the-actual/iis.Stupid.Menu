34aa678afbe237ca08afd86ce9b51cabf52e5536
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-10-22 21:39:33 -0400

    Change mod name, FINAL 7.5.1

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-10-23T01:29:45Z";
+        public const string BuildTimestamp = "2025-10-23T01:39:19Z";
         public const string Version = "7.5.1";
 
         public const string BaseDirectory = "iisStupidMenu";
