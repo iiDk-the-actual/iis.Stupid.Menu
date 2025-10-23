f2af3d02748ef838d249006709e18e2f2826884c
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-10-23 17:48:02 -0400

    Optimize

--- Patches/Menu/MicPatch.cs
+++ Patches/Menu/MicPatch.cs
@@ -31,12 +31,8 @@ namespace iiMenu.Patches.Menu
         private static bool Prefix(GorillaSpeakerLoudness __instance, ref bool ___isMicEnabled, ref bool ___isSpeaking, ref float ___loudness)
         {
             if (enabled && __instance.gameObject.name == "Local Gorilla Player")
-            {
-                ___isMicEnabled = false;
-                ___isSpeaking = false;
-                ___loudness = 0f;
                 return false;
-            }
+            
             return true;
         }
     }
