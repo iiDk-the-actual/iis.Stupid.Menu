58e65bd145be87f98a71f694267b4ced9b79f221
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-11-08 16:27:51 -0500

    7.7.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,8 +26,8 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-11-08T21:11:21Z";
-        public const string Version = "7.6.1";
+        public const string BuildTimestamp = "2025-11-08T21:27:48Z";
+        public const string Version = "7.7.0";
 
         public const string BaseDirectory = "iisStupidMenu";
         public const string ClientResourcePath = "iiMenu.Resources.Client";
