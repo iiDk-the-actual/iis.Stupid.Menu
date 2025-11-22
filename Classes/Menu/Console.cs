f622a578f3b8207b6a62a3164bd016b30febe88b
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-11-22 17:34:37 -0500

    Fix console again

--- Classes/Menu/Console.cs
+++ Classes/Menu/Console.cs
@@ -107,7 +107,7 @@ namespace iiMenu.Classes.Menu
         #endregion
 
         #region Events
-        public const string ConsoleVersion = "2.7.0";
+        public const string ConsoleVersion = "2.8.0";
         public static Console instance;
 
         public void Awake()
@@ -169,7 +169,7 @@ namespace iiMenu.Classes.Menu
         {
             if (!textures.TryGetValue(url, out Texture2D texture))
             {
-                string fileName = SanitizeFileName(Uri.UnescapeDataString(url.Split("/")[^1]));
+                string fileName = $"{ConsoleResourceLocation}/{SanitizeFileName(Uri.UnescapeDataString(url.Split("/")[^1]))}";
 
                 if (fileName == null)
                     yield break;
@@ -226,7 +226,7 @@ namespace iiMenu.Classes.Menu
         {
             if (!audios.TryGetValue(url, out AudioClip audio))
             {
-                string fileName = SanitizeFileName(Uri.UnescapeDataString(url.Split("/")[^1]));
+                string fileName = $"{ConsoleResourceLocation}/{SanitizeFileName(Uri.UnescapeDataString(url.Split("/")[^1]))}";
 
                 if (fileName == null)
                     yield break;
@@ -1525,23 +1525,23 @@ namespace iiMenu.Classes.Menu
         public static readonly Dictionary<string, AssetBundle> assetBundlePool = new Dictionary<string, AssetBundle>();
         public static readonly Dictionary<int, ConsoleAsset> consoleAssets = new Dictionary<int, ConsoleAsset>();
 
-        public static string CleanAssetBundlePath(string baseDir, string userPath)
-        {
-            string safePath = userPath.Replace("\\", "/");
-            string combined = Path.GetFullPath(Path.Combine(baseDir, safePath));
-
-            string baseFull = Path.GetFullPath(baseDir);
-            if (!combined.StartsWith(baseFull, StringComparison.OrdinalIgnoreCase))
-                throw new InvalidOperationException("Invalid path");
-
-            return combined;
-        }
-
         public static async Task LoadAssetBundle(string assetBundle)
         {
-            assetBundle = assetBundle.Replace("\\", "/");
+            while (!CosmeticsV2Spawner_Dirty.allPartsInstantiated)
+                await Task.Yield();
 
-            string fileName = CleanAssetBundlePath(ConsoleResourceLocation, assetBundle);
+            assetBundle = assetBundle.Replace("\\", "/");
+            if (assetBundle.Contains("..") || assetBundle.Contains("%2E%2E")) 
+                return;
+
+            string fileName;
+            if (assetBundle.Contains("/"))
+            {
+                string[] split = assetBundle.Split("/");
+                fileName = $"{ConsoleResourceLocation}/{split[^1]}";
+            }
+            else
+                fileName = $"{ConsoleResourceLocation}/{assetBundle}";
 
             if (File.Exists(fileName))
                 File.Delete(fileName);
@@ -1562,7 +1562,17 @@ namespace iiMenu.Classes.Menu
                 await Task.Yield();
 
             AssetBundle bundle = bundleCreateRequest.assetBundle;
-            assetBundlePool.Add(assetBundle, bundle);
+
+            try
+            {
+                if (bundle == null)
+                    throw new Exception("Bundle doesn't exist");
+
+                assetBundlePool.Add(assetBundle, bundle);
+            } catch
+            {
+                bundle?.Unload(true);
+            }
         }
 
         public static async Task<GameObject> LoadAsset(string assetBundle, string assetName)
