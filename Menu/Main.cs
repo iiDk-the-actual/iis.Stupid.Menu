d3d20b6f08801f5cd256becd3bba2463cd86737c
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-11-26 17:08:37 -0500

    Fix macros and teleporting from moving your rig to your playspace center

--- Menu/Main.cs
+++ Menu/Main.cs
@@ -5600,6 +5600,8 @@ namespace iiMenu.Menu
         public static void TeleportPlayer(Vector3 pos, bool keepVelocity = false) // Prevents your hands from getting stuck on trees
         {
             GTPlayer.Instance.TeleportTo(World2Player(pos), GTPlayer.Instance.transform.rotation, keepVelocity);
+            VRRig.LocalRig.transform.position = pos;
+
             closePosition = Vector3.zero;
             Movement.lastPosition = Vector3.zero;
             if (VRKeyboard != null)
--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-11-26T04:03:54Z";
+        public const string BuildTimestamp = "2025-11-26T22:08:27Z";
         public const string Version = "7.8.1";
 
         public const string BaseDirectory = "iisStupidMenu";
