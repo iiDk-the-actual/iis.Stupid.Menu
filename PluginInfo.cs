7027bacf2641d0fa3033b2174238aad7b4db0259
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-12-07 19:37:45 -0500

    There's nothing wrong with me

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-12-08T00:36:06Z";
+        public const string BuildTimestamp = "2025-12-08T00:37:41Z";
         public const string Version = "7.8.1";
 
         public const string BaseDirectory = "iisStupidMenu";
