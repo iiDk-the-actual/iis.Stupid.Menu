a24d49c78ac05dd8c5f9bbab63af3a49cde6f902
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-12-12 16:10:25 -0500

    Fix timer patch

--- Patches/Menu/TimerPatch.cs
+++ Patches/Menu/TimerPatch.cs
@@ -35,7 +35,7 @@ namespace iiMenu.Patches.Menu
             if (enabled)
             {
                 oldDeltaTime = Time.fixedDeltaTime;
-                __instance.frameRateUpdated = true;
+                __instance._frameRateUpdated = true;
                 Time.fixedDeltaTime = 1 / UnityEngine.XR.XRDevice.refreshRate;
             }
         }
