be59298e3efe5b69d941d22091c4da55f073e562
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-12-12 18:30:21 -0500

    Fix disable wind barriers

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2025-12-12T23:13:47Z";
+        public const string BuildTimestamp = "2025-12-12T23:30:17Z";
         public const string Version = "7.9.0";
 
         public const string BaseDirectory = "iisStupidMenu";
