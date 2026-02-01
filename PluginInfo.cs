629069a977e309995f5a79db00e4dad5b5428579
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-01-31 21:01:19 -0600

    8.2.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,8 +26,8 @@ namespace iiMenu
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
         public const string Description = "Created by @crimsoncauldron with love <3";
-        public const string BuildTimestamp = "2026-02-01T02:58:46Z";
-        public const string Version = "8.1.0";
+        public const string BuildTimestamp = "2026-02-01T03:01:17Z";
+        public const string Version = "8.2.0";
 
         public const string BaseDirectory = "iisStupidMenu";
         public const string ClientResourcePath = "iiMenu.Resources.Client";
