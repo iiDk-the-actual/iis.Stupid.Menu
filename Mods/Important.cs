0d3be9c1088330f89af3b8c7cc281e60445fd151
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-02-21 17:44:38 -0600

    Why

--- Mods/Important.cs
+++ Mods/Important.cs
@@ -683,7 +683,6 @@ exit";
                 TPC.gameObject.transform.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>().enabled = wasenabled;
             }
         }
-#pragma warning restore CS0618 // Type or member is obsolete
 
         public static void ForceEnableHands()
         {
